2e284c32b8f4803172a092a4258ee2abfb4604e1
Brian Lam <brianlam@stanford.edu>
2015-04-30 01:53:31 -0700

    decomposed code. implemented move counter.

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,43 @@
 function Slider() {
   this.__board = $('#board');
   this.__answer = $('#answer');
+  this.__movesLabel = $("#move-counter");
+  this.__moves = 0;
   this.__sideLength = 4;
+  this.__blank = null;
 
+  this.createPieces();
+  this.createBlank();
+  this.__movesLabel.text(this.__moves);
+}
+
+Slider.prototype.movePiece = function(piece) {
+  var blank = this.__blank;
+
+  var blankData = $.extend({}, blank.data());
+  var pieceData = piece.data();
+
+  if (Math.abs(blankData.r - pieceData.r) + Math.abs(blankData.c - pieceData.c) == 1) {
+
+    var tempPosition = blank.position();
+
+    blank.css(piece.position());
+    blank.data('r', piece.data().r);
+    blank.data('c', piece.data().c);
+    piece.data('r', blankData.r);
+    piece.data('c', blankData.c);
+
+    piece.css(tempPosition);
+    this.__moves++;
+    this.__movesLabel.text(this.__moves);
+  }
+}
+
+Slider.prototype.createPieces = function() {
   var answerURL = this.__answer.css('background').replace(/.*url\(/, '').replace(/\).*/,'');
-
   var pieces = [];
+  var obj = this;
+
   for (var p = 0; p < (Math.pow(this.__sideLength, 2) - 1); p++) {
     var r = Math.floor(p / this.__sideLength);
     var c = p % this.__sideLength;
@@ -13,41 +45,27 @@ function Slider() {
     var piece = $('<div></div>');
     piece.addClass("piece");
     piece.css({
-     "background": "url(" + answerURL + ") " + c * -150 + "px " + r * -150 + "px",
-     "left": c * 150,
-     "top": r * 150
-   });
+      "background": "url(" + answerURL + ") " + c * -150 + "px " + r * -150 + "px",
+      "left": c * 150,
+      "top": r * 150
+    });
 
     piece.data('r', r);
     piece.data('c', c);
 
-    piece.click(function() {
-      debugger;
-      var blank = $("#blank");
-      var curr = $(this);
-
-      var blankData = $.extend({}, blank.data());
-      var currData = curr.data();
-
-      if (Math.abs(blankData.r - currData.r) + Math.abs(blankData.c - currData.c) == 1) {
-        
-        var tempPosition = blank.position();
-
-        blank.css(curr.position());
-        blank.data('r', curr.data().r);
-        blank.data('c', curr.data().c);
-        debugger;
-        curr.data('r', blankData.r);
-        curr.data('c', blankData.c);
-        
-        curr.css(tempPosition);
-      }
-      debugger;
-    });
-
+    pieces.push(piece);
     this.__board.append(piece);
   }
 
+
+  pieces.forEach(function(piece) {
+    var obj = this;
+    piece.click(function() { obj.movePiece(piece); });
+  }, obj);
+}
+
+// create blank piece as the placeholder
+Slider.prototype.createBlank = function() {
   var blankPiece = $('<div></div>');
   blankPiece.addClass('piece');
   blankPiece.attr('id', 'blank');
@@ -59,8 +77,6 @@ function Slider() {
   blankPiece.data('r', this.__sideLength - 1);
   blankPiece.data('c', this.__sideLength - 1);
 
+  this.__blank = blankPiece;
   this.__board.append(blankPiece);
-  
-
-  // create blank piece as the placeholder
 }
